94e0b817a7063bbbc3413e15c509251afea9ac11
jamoliddin007 <jamoliddin_007>
2025-05-19 22:47:32 +0500

    Update core URLs and add common api_endpoints __init__.py

---
+++ common/api_endpoints/__init__.py
@@ -0,0 +1,5 @@
+from .MediaFileCrud.MediaFileCreate import *
+from .MediaFileCrud.MediaFileUpdate import *
+from .MediaFileCrud.MediaFileDelete import *
+from .MediaFileCrud.MediaFileList import *
+from .MediaFileCrud.MediaFileDetail import *
