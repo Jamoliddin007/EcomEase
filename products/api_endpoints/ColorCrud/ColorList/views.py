ac6f66077fa27b6ae30842cc79bd0a8253dfc5ab
jamoliddin007 <jamoliddin_007>
2025-05-19 21:59:50 +0500

    Add ColorList API endpoint

---
+++ products/api_endpoints/ColorCrud/ColorList/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from products.models import Color
+from products.api_endpoints.ColorCrud.ColorList.serializers import ColorListSerializer
+
+
+class ColorListAPIView(APIView):
+    def get(self, request):
+        colors = Color.objects.all()
+        serializer = ColorListSerializer(colors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
