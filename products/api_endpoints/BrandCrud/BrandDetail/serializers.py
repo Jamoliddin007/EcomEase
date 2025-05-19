39e70fd2d6d0520fa3fff47357399d515c8b61f7
jamoliddin007 <jamoliddin_007>
2025-05-19 18:23:35 +0500

    BrandDetail serializers is added

--- products/api_endpoints/BrandCrud/BrandDetail/serializers.py
+++ products/api_endpoints/BrandCrud/BrandDetail/serializers.py
@@ -5,10 +5,12 @@ class ProductNestedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'slug', 'is_active']
+        ref_name = "BrandDetailProductNestedSerializer_ProductNested"
 
 class BrandDetailSerializer(serializers.ModelSerializer):
-    products = ProductNestedSerializer(many=True, read_only=True, source='product_set')  
+    products = ProductNestedSerializer(many=True, read_only=True, source='product_set')
 
     class Meta:
         model = Brand
         fields = ['id', 'name', 'slug', 'logo', 'products']
+        ref_name = "BrandDetailSerializer"
