24ff46a33f43f21d1977caa68eda00b41b407ae5
jamoliddin007 <jamoliddin_007>
2025-05-19 22:16:47 +0500

    Add SizeDetail API endpoint with serializer and view

---
+++ products/api_endpoints/SizeCrud/SizeDetail/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from products.models import Size
+from products.api_endpoints.SizeCrud.SizeDetail.serializers import SizeDetailSerializer
+
+class SizeDetailAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            size = Size.objects.get(pk=pk)
+        except Size.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = SizeDetailSerializer(size)
+        return Response(serializer.data)
