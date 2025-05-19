f0a2109748eccadcb15579624ce0a224fe5e61bd
jamoliddin007 <jamoliddin_007>
2025-05-19 16:29:44 +0500

    Category delete url is added

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ from products.api_endpoints import *
 urlpatterns = [
     path('categories/', CategoryListCreateAPIView.as_view(), name='category-list-create'),
     path('categories/<slug:slug>/', CategoryDetailAPIView.as_view(), name='category-detail'),
+    path('categories/<int:id>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
 ]
