6761c5073b591939ee8bf450df831be2b08b64af
jamoliddin007 <jamoliddin_007>
2025-05-19 22:47:58 +0500

    Update core URL configurations

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('products/', include('products.urls')),
+    path('common/', include('common.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 
