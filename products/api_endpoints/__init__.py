79f7ab0c619e69dcdc4c10ef93260d691b34219b
jamoliddin007 <jamoliddin_007>
2025-05-19 18:24:52 +0500

    urls is fixed and media

--- products/api_endpoints/__init__.py
+++ products/api_endpoints/__init__.py
@@ -1,3 +1,5 @@
 from .CategoryCrud.CategoryList import *
 from .CategoryCrud.CategoryDetail import *
 from .CategoryCrud.CategoryDelete import *
+from .BrandCrud.BrandDetail import *
+from .BrandCrud.BrandList import *
--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('categories/', CategoryListCreateAPIView.as_view(), name='category-list-create'),
     path('categories/<slug:slug>/', CategoryDetailAPIView.as_view(), name='category-detail'),
     path('categories/<int:id>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('brands/<int:id>/', BrandDetailView.as_view(), name='brand-detail'),
+    path('brands/', BrandListView.as_view(), name='brand-list-create'),
+    
 ]
