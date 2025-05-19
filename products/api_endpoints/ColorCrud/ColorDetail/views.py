d2008bebf4aa2fb7f392aa8ce1fb476337e7ac5f
jamoliddin007 <jamoliddin_007>
2025-05-19 21:59:48 +0500

    Add ColorDetail API endpoint

---
+++ products/api_endpoints/ColorCrud/ColorDetail/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from products.models import Color
+from products.api_endpoints.ColorCrud.ColorDetail.serializers import ColorDetailSerializer
+
+
+class ColorDetailAPIView(APIView):
+    def get(self, request, id):
+        try:
+            color = Color.objects.get(id=id)
+        except Color.DoesNotExist:
+            return Response({'detail': 'Color not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ColorDetailSerializer(color)
+        return Response(serializer.data)
