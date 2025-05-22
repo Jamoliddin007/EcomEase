a91c7bb366b5c7341c2ebcc326707381188924a9
jamoliddin007 <jamoliddin_007>
2025-05-22 19:11:38 +0500

    Update blog URLs and views in common app

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from common.api_endpoints import *
+from common.views import *
+app_name = 'common'
 
 urlpatterns = [
     path('mediafiles/', MediaFileListAPIView.as_view(), name='mediafile-list'),
@@ -7,4 +9,14 @@ urlpatterns = [
     path('mediafiles/<int:id>/', MediaFileDetailAPIView.as_view(), name='mediafile-detail'),
     path('mediafiles/<int:id>/update/', MediaFileUpdateAPIView.as_view(), name='mediafile-update'),
     path('mediafiles/<int:id>/delete/', MediaFileDeleteAPIView.as_view(), name='mediafile-delete'),
+    path('index/', HomeView.as_view(), name='home'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('blog-details/', BlogDetailView.as_view(), name='blog-details'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('main/', MainView.as_view(), name='main'),
+    path('shop-details/', ShopDetailView.as_view(), name='shop-details'),
+    path('shop-grid/', ShopGridView.as_view(), name='shop-grid'),
+    path('shoping-cart/', ShopingCartView.as_view(), name='shoping-cart'),
+
 ]
--- common/views.py
+++ common/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView # htmlni render qilib beradi template larni view qilib beradi
+from products.models import Category,Product
+class HomeView(TemplateView):
+    template_name = 'index.html'
 
-# Create your views here.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+
+
+        context['title'] = 'VooComerce'
+        context['categories'] = categories
+        return context
+    
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us'
+        return context
+    
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Blog'
+        return context
+    
+class BlogDetailView(TemplateView):
+    template_name = 'blog-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Blog Detail'
+        return context
+    
+class CheckoutView(TemplateView):
+    template_name = 'checkout.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Checkout'
+        return context
+    
+class MainView(TemplateView):
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main'
+        return context
+    
+class ShopDetailView(TemplateView):
+    template_name = 'shop-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Shop Detail'
+        return context
+    
+class ShopGridView(TemplateView):
+    template_name = 'shop-grid.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Shop Grid'
+        return context
+    
+class ShopingCartView(TemplateView):
+    template_name = 'shoping-cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Shopping Cart'
+        return context
