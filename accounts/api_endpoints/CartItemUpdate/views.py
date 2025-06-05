e4b5ec26c0a65d53c260e0adca5228011232d38a
jamoliddin007 <jamoliddin_007>
2025-06-05 11:40:37 +0500

    For CartItemUpdate serializers and views are done

--- accounts/api_endpoints/CartItemUpdate/views.py
+++ accounts/api_endpoints/CartItemUpdate/views.py
@@ -1,26 +1,16 @@
-from rest_framework.generics import GenericAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework import permissions
-from rest_framework.response import Response
 
-from accounts.api_endpoints.CartItemCreate.serializers import CartItemCreateSerializer
 from accounts.models import CartItem
+from accounts.api_endpoints.CartItemUpdate.serializers import CartItemUpdateSerializer
 
-class CartItemsCreateAPIView(GenericAPIView):
+
+class CartItemsUpdateAPIView(GenericAPIView):
     queryset = CartItem.objects.all()
-    serializer_class = CartItemCreateSerializer
+    serializer_class = CartItemUpdateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-        return Response(serializer.data)
-    
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-__all__ = ["CartItemsCreateAPIView"]
+__all__ = ["CartItemsUpdateAPIView"]
