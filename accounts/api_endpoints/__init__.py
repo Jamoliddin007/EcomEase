dcc7f4b3aca6afa15755453209fe05656ac7058c
jamoliddin007 <jamoliddin_007>
2025-06-05 11:49:10 +0500

    Profile folder all inside folders are done!

--- accounts/api_endpoints/__init__.py
+++ accounts/api_endpoints/__init__.py
@@ -1,6 +1,6 @@
 from .LoginSession import *
 from .LogoutSession import *
-from .CartItemsList import *
+from .CartItemList import *
 from .CartItemCreate import *
 from .CartItemUpdate import *
 from .CartItemDelete import *
