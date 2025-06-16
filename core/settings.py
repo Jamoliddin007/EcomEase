1132efeb209e9ce7a0764d6526f97829dd846e12
jamoliddin007 <jamoliddin_007>
2025-06-16 16:47:44 +0500

    fixed problems ans addes celery to the project

--- core/settings.py
+++ core/settings.py
@@ -180,3 +180,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Celery Configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
