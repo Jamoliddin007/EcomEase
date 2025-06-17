6789ff2e944a228774ce3ec842db1ee31947f4a8
jamoliddin007 <jamoliddin_007>
2025-06-17 10:14:17 +0500

    _init_.py is added

--- accounts/api_endpoints/__init__.py
+++ accounts/api_endpoints/__init__.py
@@ -1,6 +1,6 @@
 from .LoginSession import *
 from .LogoutSession import *
-from .CartItemList import *
+from .CartItemsList import *
 from .CartItemCreate import *
 from .CartItemUpdate import *
 from .CartItemDelete import *
