903b71c9d5224f7006b2bd41ded1fe57753dce69
jamoliddin007 <jamoliddin_007>
2025-06-17 19:08:37 +0500

    CategoryDelete -> serializers and views are done

--- products/api_endpoints/Category/CategoryDelete/serializers.py
+++ products/api_endpoints/Category/CategoryDelete/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+
 from products.models import Category
+
 
 class CategoryDeleteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']  
+        fields = ["id"]
