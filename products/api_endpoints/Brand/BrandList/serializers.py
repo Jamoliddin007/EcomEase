553ef61b2938259ba22ad61af25428713bb8fb93
jamoliddin007 <jamoliddin_007>
2025-06-17 17:24:53 +0500

    BrandList -> serializers and views are added

--- products/api_endpoints/Brand/BrandList/serializers.py
+++ products/api_endpoints/Brand/BrandList/serializers.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
-from products.models import Brand, Product
 
-class ProductNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'slug', 'is_active']
-        ref_name = "BrandListProductNestedSerializer_ProductNested"
+from products.models import Brand
+
 
 class BrandListSerializer(serializers.ModelSerializer):
-    products = ProductNestedSerializer(many=True, read_only=True, source='product_set')
-
     class Meta:
         model = Brand
-        fields = ['id', 'name', 'slug', 'logo', 'products']
-        ref_name = "BrandListSerializer"
+        fields = [
+            "id",
+            "name",
+            "slug"
+        ]
--- products/api_endpoints/Brand/BrandList/views.py
+++ products/api_endpoints/Brand/BrandList/views.py
@@ -1,7 +1,18 @@
-from rest_framework.generics import ListAPIView
-from products.models import Brand
-from .serializers import BrandListSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
 
-class BrandListView(ListAPIView):
+from products.models import Brand
+from products.api_endpoints.Brand.BrandList.serializers import BrandListSerializer
+
+
+class BrandListAPIView(ListAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BrandRetrieveAPIView(RetrieveAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = "slug"
