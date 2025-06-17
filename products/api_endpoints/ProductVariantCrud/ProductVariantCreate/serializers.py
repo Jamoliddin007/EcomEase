7769a444feac8b5dbe2773bea608f707e749c4b7
jamoliddin007 <jamoliddin_007>
2025-06-17 17:15:04 +0500

    renamed all folders without crud

--- products/api_endpoints/ProductVariantCrud/ProductVariantCreate/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from products.models import ProductVariant
-
-class ProductVariantCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductVariant
-        fields = ['name', 'price', 'product', 'images', 'stock', 'color', 'size', 'is_active']
--- products/api_endpoints/ProductVariantCrud/ProductVariantDetail/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from products.models import ProductVariant
-
-class ProductVariantDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductVariant
-        fields = ['id', 'name', 'price', 'product', 'images', 'stock', 'color', 'size', 'is_active']
