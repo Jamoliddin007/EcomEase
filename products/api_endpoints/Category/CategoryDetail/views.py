b24602448cbc4d67795359964f1a4056b44f775a
jamoliddin007 <jamoliddin_007>
2025-06-18 14:11:10 +0500

    Remove unused CategoryDetail endpoint

--- products/api_endpoints/Category/CategoryDetail/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import RetrieveAPIView
-from products.models import Category
-from products.api_endpoints.Category.CategoryDetail.serializers import CategoryDetailSerializer
-
-class CategoryDetailAPIView(RetrieveAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategoryDetailSerializer
-    lookup_field = 'id'  
