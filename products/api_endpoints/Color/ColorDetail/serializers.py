51ec87bc6106949b5dac3304bcece460d859b2b4
jamoliddin007 <jamoliddin_007>
2025-06-24 17:34:06 +0500

    refactor: cleanup and restructure product CRUD endpoints (Color, Size, etc.)

--- products/api_endpoints/Color/ColorDetail/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from products.models import Color
-
-class ColorDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Color
-        fields = ['id', 'name', 'slug']
--- products/api_endpoints/Size/SizeDetail/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from products.models import Size
-
-class SizeDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Size
-        fields = ['id', 'name', 'slug']
