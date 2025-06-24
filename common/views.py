39ba2dcefe29e3ab0909169c9cbf1c8fe69ce946
jamoliddin007 <jamoliddin_007>
2025-06-24 16:32:56 +0500

    refactor: clean up common APIs and update media, review, register endpoints

--- common/views.py
+++ common/views.py
@@ -1,78 +1,79 @@
-from django.views.generic import TemplateView # htmlni render qilib beradi template larni view qilib beradi
-from products.models import Category,Product
+from django.views.generic import TemplateView
+from django.db import models
+
+from products.models import Category
+from accounts.models import CartItem
+
+
 class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         categories = Category.objects.all()
 
-
-        context['title'] = 'VooComerce'
-        context['categories'] = categories
+        context["title"] = "VooCommerce | Home"
+        context["categories"] = categories
+        print(categories[1].image.url)
         return context
-    
+
+
 class ContactView(TemplateView):
-    template_name = 'contact.html'
+    template_name = "contact.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Contact Us'
+        context["title"] = "VooCommerce | Contact Us"
         return context
-    
+
 
 class BlogView(TemplateView):
-    template_name = 'blog.html'
+    template_name = "blog.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Blog'
-        return context
-    
+        return super().get_context_data(**kwargs)
+
+
 class BlogDetailView(TemplateView):
-    template_name = 'blog-details.html'
+    template_name = "blog-details.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Blog Detail'
-        return context
-    
-class CheckoutView(TemplateView):
-    template_name = 'checkout.html'
+        context["title"] = "VooCommerce | Blog Detail"
+        return super().get_context_data(**kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Checkout'
-        return context
-    
-class MainView(TemplateView):
-    template_name = 'main.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Main'
-        return context
-    
-class ShopDetailView(TemplateView):
-    template_name = 'shop-details.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Shop Detail'
-        return context
-    
 class ShopGridView(TemplateView):
-    template_name = 'shop-grid.html'
+    template_name = "shop-grid.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Shop Grid'
-        return context
-    
+        return super().get_context_data(**kwargs)
+
+
+class ShopDetailsView(TemplateView):
+    template_name = "shop-details.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
 class ShoppingCartView(TemplateView):
-    template_name = 'shoping-cart.html'
+    template_name = "shopping-cart.html"
 
     def get_context_data(self, **kwargs):
+        cart_items = CartItem.objects.filter(cart=self.request.user.cart).annotate(
+            total_amount=models.F("quantity") * models.F("product__price")
+        )
+
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Shopping Cart'
+        context["cartitems"] = cart_items
+
         return context
+
+
+class CheckoutView(TemplateView):
+    template_name = "checkout.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
